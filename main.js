dc9bfbce8e1f178832ed4d598954310b3744e4b1
sivaaravindh <sivaaravind262003@gmail.com>
2022-12-20 19:46:21 -0800

    index

---
+++ main.js
@@ -0,0 +1,50 @@
+let fruit={
+  mango:"yellow",
+  orange:"orange",
+  apple:"red",
+  graps:"violet",
+  greenapple:"green",
+}
+
+console.log(fruit);
+
+let festivel={
+  janauary:"pongal",
+  june:"ramjone",
+  october:"devali",
+  december:"christhumas",
+}
+
+console.log(festivel);
+
+let football={
+  argentina:"frist",
+  france:"secound",
+  morichius:"third",
+  morraco:"furth",
+}
+
+console.log(football)
+
+let language={
+  tamil:5000,
+  samskrit:4000,
+  telngu:3000,
+  english:1500,
+
+}
+
+console.log(language)
+
+let subject={
+      Tamil:90,
+      English:90,
+      Maths:85,
+      elctricity:90,
+      atomicPhysics:100,
+}
+
+console.log(subject)
+
+
+
